2516b58ec5b2f219a37d1dbe66e6f6bca6aed8de
Chaitanya Pyla <128012474+Chaitanya4633@users.noreply.github.com>
2025-07-21 06:45:20 +0530

    Update LoanApplication.java

--- Week 05 - Spring Boot 3 And Spring Cloud/Microservices/loan/src/main/java/com/cognizant/loan/LoanApplication.java
+++ Week 05 - Spring Boot 3 And Spring Cloud/Microservices/loan/src/main/java/com/cognizant/loan/LoanApplication.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
 @RestController
 @RequestMapping("/loans")
-public class LoanApplication {
+public class LoanApplication 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         SpringApplication.run(LoanApplication.class, args);
     }
 
@@ -18,19 +20,32 @@ public class LoanApplication {
     //     return new Loan(number, "home", 800000.0);
     // }
 
-    static class Loan {
+    static class Loan 
+    {
         private String number;
         private String type;
         private double amount;
 
-        public Loan(String number, String type, double amount) {
-            this.number = number;
-            this.type = type;
-            this.amount = amount;
+        public Loan(String number,String type,double amount) 
+        {
+            this.number=number;
+            this.type=type;
+            this.amount=amount;
         }
 
-        public String getNumber() { return number; }
-        public String getType() { return type; }
-        public double getAmount() { return amount; }
+        public String getNumber() 
+        { 
+            return number;
+        }
+        
+        public String getType() 
+        { 
+            return type; 
+        }
+        
+        public double getAmount() 
+        { 
+            return amount; 
+        }
     }
 }
