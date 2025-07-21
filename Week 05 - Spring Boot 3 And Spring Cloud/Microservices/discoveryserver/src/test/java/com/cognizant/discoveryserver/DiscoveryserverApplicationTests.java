5cfdfc98208bedcb26a0223c5347711b4b1c1a21
Chaitanya Pyla <128012474+Chaitanya4633@users.noreply.github.com>
2025-07-21 06:43:33 +0530

    Update DiscoveryserverApplicationTests.java

--- Week 05 - Spring Boot 3 And Spring Cloud/Microservices/discoveryserver/src/test/java/com/cognizant/discoveryserver/DiscoveryserverApplicationTests.java
+++ Week 05 - Spring Boot 3 And Spring Cloud/Microservices/discoveryserver/src/test/java/com/cognizant/discoveryserver/DiscoveryserverApplicationTests.java
@@ -1,13 +1,9 @@
 package com.cognizant.discoveryserver;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
-class DiscoveryserverApplicationTests {
-
+class DiscoveryserverApplicationTests 
+{
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
