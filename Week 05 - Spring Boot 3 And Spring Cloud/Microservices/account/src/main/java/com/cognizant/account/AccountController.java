8780a00cadcd0d46fa1586bf7348a261d50fb413
Chaitanya Pyla <128012474+Chaitanya4633@users.noreply.github.com>
2025-07-21 06:27:41 +0530

    Update AccountController.java

--- Week 05 - Spring Boot 3 And Spring Cloud/Microservices/account/src/main/java/com/cognizant/account/AccountController.java
+++ Week 05 - Spring Boot 3 And Spring Cloud/Microservices/account/src/main/java/com/cognizant/account/AccountController.java
@@ -4,26 +4,39 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
-public class AccountController {
-
+public class AccountController 
+{
     @GetMapping("/{id}")
-    public Account getAccount(@PathVariable String id) {
-        return new Account(id, "Chaitanya", 99999);
+    public Account getAccount(@PathVariable String id) 
+    {
+        return new Account(id,"Chaitanya",99999);
     }
 
-    static class Account {
+    static class Account 
+    {
         private String id;
         private String name;
         private int balance;
 
-        public Account(String id, String name, int balance) {
-            this.id = id;
-            this.name = name;
-            this.balance = balance;
+        public Account(String id,String name,int balance) 
+        {
+            this.id=id;
+            this.name=name;
+            this.balance=balance;
         }
 
-        public String getId() { return id; }
-        public String getName() { return name; }
-        public int getBalance() { return balance; }
+        public String getId() 
+        { 
+            return id; 
+        }
+        
+        public String getName() 
+        { 
+            return name; 
+        }
+        public int getBalance() 
+        { 
+            return balance; 
+        }
     }
 }
